d9786a9311086e772116f9e3cb91c04c962f0df0
Gautierpicon <gautierpicon@proton.me>
2025-03-21 21:00:09 +0100

    header improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 export default function Header() {
     return (
-        <div className="flex items-center mb-8 group">
-            <div className="relative h-16 w-16 mr-3">
-                <img 
-                    src="/logo.svg"
-                    alt="Raccoon.ai Logo" 
-                    className="h-16 w-16 transform transition-transform duration-300 hover:rotate-20"
-                />
-            </div>
-            <h1 className="bricolage-font text-5xl bg-clip-text text-transparent bg-amber-500">
-                Raccoon.ai
-            </h1>
+        <div className="flex items-center mb-8">
+            <a href="https://github.com/Gautierpicon/RaccoonAI" target="_blank" className="flex items-center group">
+                <div className="relative h-16 w-16 mr-3">
+                    <img
+                        src="/logo.svg"
+                        alt="Raccoon.ai Logo"
+                        className="h-16 w-16 transform transition-transform duration-300 group-hover:rotate-20"
+                    />
+                </div>
+                <h1 className="bricolage-font text-5xl bg-clip-text text-amber-500 group-hover:underline">
+                    Raccoon.ai
+                </h1>
+            </a>
         </div>
     );
 }
