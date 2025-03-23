f0662c3b7fc62ceeac3bbbbad87d64b20b4b9a8e
Gautierpicon <gautierpicon@proton.me>
2025-03-23 16:13:14 +0100

    bug fixed

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -7,7 +7,7 @@ export default function InputArea({
     onClear
   }) {
     return (
-      <div className="px-4 pt-4 pb-2 rounded-xl border-gray-200 shadow-lg group focus-within:border-emerald-700 focus-within:ring-2 focus-within:ring-emerald-700/30 transition-all duration-200">
+      <div className="px-4 pt-4 pb-2 rounded-xl border border-gray-200 shadow-lg group focus-within:border-emerald-700 focus-within:ring-2 focus-within:ring-emerald-700/30 transition-all duration-200">
         <div className="relative">
           <textarea
             value={input}
